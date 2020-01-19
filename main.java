4d468a931560d5c6c66d19dc4c50fe4b4c6bd4b5
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-01-20 00:07:30 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,13 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package hms;
-
-/**
- *
- * @author tosin
+ * main class provided for testing and printing to console
  */
 public class main {
     
