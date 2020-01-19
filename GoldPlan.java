64f69c681286f506313fd3a3f9d6b9e5f888a38d
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-01-19 23:50:30 +0100

    Update GoldPlan.java

--- GoldPlan.java
+++ GoldPlan.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * A subclass of class HealthInsurancePlan with coverage of 0.8
  */
-package hms;
 
-/**
- *
- * @author tosin
- */
+
 public class GoldPlan extends HealthInsurancePlan {
     
     GoldPlan () {
