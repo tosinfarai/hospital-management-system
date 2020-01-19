4683a2f8b8ec0048ac18545b639a904e5bdf21d0
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-01-19 23:49:20 +0100

    Update BronzePlan.java

--- BronzePlan.java
+++ BronzePlan.java
@@ -1,14 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * A subclass of class HealthInsurancePlan with coverage of 0.6
  */
-package hms;
 
-/**
- *
- * @author tosin
- */
 public class BronzePlan extends HealthInsurancePlan {
     
     BronzePlan () {
