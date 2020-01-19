8555c9f99150fdc9f9b29bbdf9c2a263ec132b61
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-01-20 00:02:10 +0100

    Update User.java

--- User.java
+++ User.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Superclass encompassing all users of the hospital.
  */
-package hms;
 
-/**
- *
- * @author tosin
- */
+
 public class User {
     
 	private long id;
