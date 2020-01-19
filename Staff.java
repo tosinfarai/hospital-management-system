a12029be1e85b8ae35566ab26626e9cc4b6b11d8
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-01-19 23:58:42 +0100

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * A subclass of class User
  */
-package hms;
 
-/**
- *
- * @author tosin
- */
+
 public class Staff extends User{
     
     private long staffId;
