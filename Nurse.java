e672e03f32ce8c643b3b202174bd880fcdafa403
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-01-19 23:57:26 +0100

    Update Nurse.java

--- Nurse.java
+++ Nurse.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * A subclass of class Staff
  */
-package hms;
 
-/**
- *
- * @author tosin
- */
+
 public class Nurse extends Staff{
     
     private long nurseId;
