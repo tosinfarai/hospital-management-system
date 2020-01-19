7a0f803bc542e0b31649ca27208c2825e81e95cf
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-01-19 23:53:36 +0100

    Update SilverPlan.java

--- SilverPlan.java
+++ SilverPlan.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * A subclass of class HealthInsurancePlan with coverage of 0.7
  */
-package hms;
 
-/**
- *
- * @author tosin
- */
+
 public class SilverPlan extends HealthInsurancePlan {
     
     SilverPlan () {
