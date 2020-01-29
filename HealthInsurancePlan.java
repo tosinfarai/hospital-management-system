f15045b597b9e3854d1e8076a8052a2d0ac62289
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-01-29 21:59:57 +0100

    Update HealthInsurancePlan.java

--- HealthInsurancePlan.java
+++ HealthInsurancePlan.java
@@ -7,12 +7,23 @@
 public class HealthInsurancePlan {
     
     double coverage; 
+    private InsuranceBrand offeredBy;
     
     public double getCoverage () {
         return coverage;
     }
     public void setCoverage (double coverage) {
         this.coverage = coverage;
+    }
+    
+    public abstract double computeMonthlyPremium (double salary);
+    
+    public InsuranceBrand getOfferedBy() {
+	return offeredBy;
+    }
+
+    public void setOfferedBy(InsuranceBrand offeredBy) {
+	this.offeredBy = offeredBy;
     }
     
 }
