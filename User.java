b15e2c6263cae2eaf651f170ea345afa5cf84036
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-01-29 22:08:38 +0100

    Update User.java

--- User.java
+++ User.java
@@ -5,45 +5,61 @@
 
 public class User {
     
-	private long id;
-	private String firstName;
-	private String lastName;
-	private String gender;
-	private String email;
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String email;
+    private boolean insured;
+    private HealthInsurancePlan insurancePlan; 
 	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
+    public long getId() {
+	return id;
+    }
+    public void setId(long id) {
+	this.id = id;
+    }
 	
-	public String getFirstName() {
-	    return firstName;
-	}
-	public void setFirstName(String firstName) {
-	    this.firstName = firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 	
-	public String getLastName() {
-	    return firstName;
-	}
-	public void setLastName(String lastName) {
-	    this.lastName = lastName;
-	}
+    public String getLastName() {
+        return firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
         
-        public String getGender() {
-	    return firstName;
-	}
-	public void setGender(String gender) {
-	    this.gender = gender;
-	}
+    public String getGender() {
+        return firstName;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
         
-        public String getEmail() {
-	    return email;
-	}
-	public void setEmail(String email) {
-	    this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+        
+    public boolean isInsured () {
+        return insured;
+    }
+    public void setInsured (boolean insured) {
+        this.insured = insured;
+    }
+    
+    public HealthInsurancePlan getInsurancePlan () {
+        return insurancePlan;
+    }
+    public void setInsurancePlan (HealthInsurancePlan insurancePlan) {
+        this.insurancePlan = insurancePlan;
+    }
 	
 }
