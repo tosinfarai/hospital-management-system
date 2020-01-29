490c4fceb0c13b35053bc1fa8f5cd2a9040f3dee
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-01-29 21:58:41 +0100

    Update GoldPlan.java

--- GoldPlan.java
+++ GoldPlan.java
@@ -8,5 +8,11 @@ public class GoldPlan extends HealthInsurancePlan {
     GoldPlan () {
         coverage = 0.8;
     }
+
+    @Override
+    public double computeMonthlyPremium (double salary){
+       double monthlyPremium = 0.07 * salary;
+       return monthlyPremium;
+    }
     
 }
